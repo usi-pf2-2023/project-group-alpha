8e6f14332af964baa0ea099fef8dd0173628f06d
Boboge <50315826+avgstuBoboge@users.noreply.github.com>
2023-05-12 23:27:34 +0200

    feature: add some properties of enum Kind. Now it allows you to get the Graphic map according to the Kind.

--- src/app/game/state/Kind.java
+++ src/app/game/state/Kind.java
@@ -1,10 +1,49 @@
 package src.app.game.state;
 
+import jtamaro.en.Graphic;
+import src.app.game.graphs.GameGraphics;
+
+import java.util.HashMap;
+
 public enum Kind {
-    BOUNDARY,
-    ICON_WALL, TEXT_WALL,
-    ICON_BABA, TEXT_BABA,
-    ICON_FLAG, TEXT_FLAG,
-    ICON_ROCK, TEXT_ROCK,
-    TEXT_IS, TEXT_WIN, TEXT_PUSH, TEXT_YOU, TEXT_STOP
+    BOUNDARY(GameGraphics.BOUNDARY, false, false),
+
+    ICON_WALL(GameGraphics.ICON_WALL, false, false),
+    ICON_BABA(GameGraphics.ICON_BABA, false, false),
+    ICON_FLAG(GameGraphics.ICON_FLAG, false, false),
+    ICON_ROCK(GameGraphics.ICON_ROCK, false, false),
+
+    TEXT_IS(GameGraphics.TEXT_IS, false, false),
+
+    TEXT_WALL(GameGraphics.TEXT_WALL, true, false),
+    TEXT_BABA(GameGraphics.TEXT_BABA, true, false),
+    TEXT_FLAG(GameGraphics.TEXT_FLAG, true, false),
+    TEXT_ROCK(GameGraphics.TEXT_ROCK, true, false),
+
+    TEXT_WIN(GameGraphics.TEXT_WIN, false, true),
+    TEXT_PUSH(GameGraphics.TEXT_PUSH, false, true),
+    TEXT_YOU(GameGraphics.TEXT_YOU, false, true),
+    TEXT_STOP(GameGraphics.TEXT_STOP, false, true);
+
+    private HashMap<String, Graphic> graphic_map;
+    private boolean isObjectText;
+    private boolean isStateText;
+
+    Kind(HashMap<String, Graphic> graphic_map, boolean isObjectText, boolean isStateText) {
+        this.graphic_map = graphic_map;
+        this.isObjectText = isObjectText;
+        this.isStateText = isStateText;
+    }
+
+    public HashMap<String, Graphic> getGraphic_map() {
+        return graphic_map;
+    }
+
+    public boolean isObjectText() {
+        return isObjectText;
+    }
+
+    public boolean isStateText() {
+        return isStateText;
+    }
 }
