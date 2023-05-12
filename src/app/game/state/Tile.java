84130129e59d33098049824383ca1e50b6c292b2
Boboge <50315826+avgstuBoboge@users.noreply.github.com>
2023-05-12 23:28:20 +0200

    feature: add some check function of Tile class.

--- src/app/game/state/Tile.java
+++ src/app/game/state/Tile.java
@@ -11,15 +11,59 @@ public record Tile(Sequence<Item> items) {
     // convert a Tile to a Graphic
     public Graphic toGraphic() {
         return reduce(
-            (g, item) -> overlay(g, item.toGraphic()),
+            (graphic, item) -> overlay(graphic, item.toGraphic()),
             emptyGraphic(),
             items
         );
     }
+
     // check whether it's possible to step on this Tile
     public boolean canStepOn() {
-        // TODO: implementation
+        for (Item item : items) {
+            if (item.stop() || item.name() == Kind.BOUNDARY) {
+                return false;
+            }
+        }
         return true;
     }
+
+    // support to assert, only used here?
+    public static <T> int size(Sequence<T> seq) {
+        return isEmpty(seq) ? 0 : 1 + size(rest(seq));
+    }
+
+    // return whether this tile contains TEXT_IS
+    public boolean containsIs() {
+        for (Item item : items) {
+            if (item.name() == Kind.TEXT_IS) {
+                assert size(items) == 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // return whether this tile contains any object text
+    public boolean containsObjectText() {
+        for (Item item : items) {
+            if (item.name().isObjectText()) {
+                assert size(items) == 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // return whether this tile contains any state text
+    public boolean containsStateText() {
+        for (Item item : items) {
+            if (item.name().isStateText()) {
+                assert size(items) == 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
