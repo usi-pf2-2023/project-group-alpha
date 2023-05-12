a526a1e9db91f41a75fa8d2783c3f31e441516d1
Boboge <50315826+avgstuBoboge@users.noreply.github.com>
2023-05-12 23:23:07 +0200

    feature: improvement for the structure. Move the render to an independent package called "views".

---
+++ src/app/game/view/GameView.java
@@ -0,0 +1,28 @@
+package src.app.game.view;
+
+import jtamaro.en.Graphic;
+import src.app.game.Settings;
+import src.app.game.state.GameState;
+import src.app.game.state.Tile;
+
+import java.util.ArrayList;
+
+import static jtamaro.en.Colors.BLACK;
+import static jtamaro.en.Graphics.*;
+
+public class GameView {
+    public static Graphic render(GameState gameState) {
+        ArrayList<ArrayList<Tile>> map = gameState.getMap();
+        int n = map.size(), m = map.get(0).size();
+        Graphic fore = emptyGraphic();
+        for (int i = 0; i < n; i++) {
+            Graphic rowFore = emptyGraphic();
+            for (int j = 0; j < m; j++) {
+                rowFore = beside(rowFore, map.get(i).get(j).toGraphic());
+            }
+            fore = above(fore, rowFore);
+        }
+        Graphic back = rectangle(m * Settings.UNIT_WIDTH, n * Settings.UNIT_HEIGHT, BLACK);
+        return overlay(fore, back);
+    }
+}
