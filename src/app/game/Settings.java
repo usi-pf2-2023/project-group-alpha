5f152dfa4211987e683f41c3ec3c93709a375b61
Zhiyao Yan <pepperyzy@gmail.com>
2023-05-31 00:18:47 +0200

    fix: make the width and height always the same

--- src/app/game/Settings.java
+++ src/app/game/Settings.java
@@ -23,7 +23,7 @@ import java.nio.file.Path;
 public class Settings {
     //canvas settings
     public static final double UNIT_HEIGHT = 50;
-    public static final double UNIT_WIDTH = 50;
+    public static final double UNIT_WIDTH = UNIT_HEIGHT;
     // the totalLevel should realize automatic calculation!!!
     public static final int totalLevel = 3;
     public static GameState initialState;
