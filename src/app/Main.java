6f6a443a057a0c77f493ea2b9374c2e34f8626d9
Zhiyao Yan <pepperyzy@gmail.com>
2023-05-30 23:48:43 +0200

    fix: optimization of interaction

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,18 @@
 package src.app;
 
-import src.app.game.GameRun;
+import jtamaro.en.IO;
 import src.app.game.Settings;
+import src.app.game.controller.GameController;
+import src.app.game.view.GameView;
+import src.app.game.view.graphs.ImageConverter;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        GameRun.run();
+        IO.interact(Settings.initialState)
+          .withBackground(
+              ImageConverter.toHints("mainBackground.png", 39.52 * Settings.UNIT_WIDTH, 27.52 * Settings.UNIT_HEIGHT))
+          .withRenderer(gameState -> GameView.render(gameState))
+          .withKeyPressHandler(GameController::onKeyPress)
+          .run();
     }
 }
