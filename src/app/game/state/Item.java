b1da0258adaefdf3108b317787962b379d9a7775
IronJam <oubenj@usi.ch>
2023-06-01 10:27:55 +0200

    Feat: Updating documentation for withHeadings(), hashWall(), withHeading(), isText(), isIconWall(), isIconBaba()

--- src/app/game/state/Item.java
+++ src/app/game/state/Item.java
@@ -58,28 +58,40 @@ public record Item(Kind name, // describes what kind of object it is
     }
 
     /**
-     * isIconBaba() determines whether an object is an iconBaba.
-     *
-     * @return true if that is the case, false otherwise.
+     * Given {@code this}, determines if {@code this} is an icon of Baba (i.e {@code Kind.ICON_BABA}) or not
+     * @return {@code true} if it is the case, {@code false} otherwise.
      */
     public boolean isIconBaba() {
         return this.name().equals(Kind.ICON_BABA);
     }
-
+    /**
+     * Given {@code this}, determines if {@code this} is a wall icon (i.e {@code Kind.ICON_WALL}) or not.
+     * @return {@code true} if it is the case, {@code false} otherwise.
+     */
     public boolean isIconWall() {
         return this.name().equals(Kind.ICON_WALL);
     }
 
+    /**
+     * Given {@code this}, determines if {@code this} is a text or not.
+     * @return {@code true} if it is the case, {@code false} otherwise.
+     */
     public boolean isText() {
         return this.name().isObjectText() || this.name().isStateText() || this.name() == Kind.TEXT_IS;
     }
 
     /**
-     * Item.withHeading() creates a new Item object whose fields have the same values as the input object except for the
-     * heading field which is now equal to the new heading if the object is "you"
+     * Given a {@code Heading} creates a new {@code Item} with the same fields as {@code this} but heading equal to
+     * {@code heading} if {@code this.you()} is {@code true} (i.e if {@code this} is controlled by the player.
      *
-     * @param heading is obtained through keyboard interactions
-     * @return an Item
+     * This method is called during move operations (cf {@code GameState} class) to assess whether the heading of the
+     * object moved (i.e {@code this}), thus its appearance, must change.
+     * Currently, the only {@code Item} that sees graphical changes following this is {@code ICON_BABA}.
+     *
+     * @param heading is the {@code Heading} of the new {@code Item}. It is obtained through keyboard interactions.
+     * monitored by methods in the {@code GameController} class.
+     * @return the passed {@code Item} if that {@code Item} did not have the property {@code you} OR returns a new
+     * {@code Item} whose fields are all equal to the passed {@code Item}.
      */
     public Item withHeading(Heading heading) {
         return this.you()
@@ -88,14 +100,14 @@ public record Item(Kind name, // describes what kind of object it is
     }
 
     /**
-     * Item.hashWall() determines whether the Tile at coordinates x and y contains a wall or not.
-     * Its final goal is to hash the type of wall to a String. Ultimately, a type of wall is represented by a 4 digits
-     * String (we have 16 types of walls). The string outputted by this method is concatenated with 3 other digits in the
-     * GameView.renderWall() method.
-     * @param gameStateMap is the current GameState's map
-     * @param x is the x coordinate of the Tile we are checking
-     * @param y is the y coordinate of the Tile we are checking
-     * @return "1" if the Tile contains a wall, "0" otherwise
+     * Given an {@code Item}, determines whether the {@code Tile} at coordinates x and y contains a wall or not.
+     * Its final goal is to hash the type of wall to a String. Ultimately, a type of wall is represented by a 4 digits.
+     * {@code String} (we have 16 types of walls). The {@code String} outputted by this method is concatenated with 3 other digits in the
+     * {@code GameView.renderWall()} method.
+     * @param gameStateMap  the current {@code GameState}'s map.
+     * @param x  the x coordinate of the {@code Tile} we are checking.
+     * @param y  the y coordinate of the {@code Tile} we are checking.
+     * @return {@code "1"} if the {@code Tile} contains a wall, {@code "0"} otherwise.
      */
     public String hashWall(ArrayList<ArrayList<Tile>> gameStateMap, int x, int y) {
         // Scan all the items on the Tile
@@ -110,10 +122,12 @@ public record Item(Kind name, // describes what kind of object it is
     }
 
     /**
-     * Item.withHeadings() changes the Items in a Sequence correspondingly to a new Heading
-     * @param heading is the Heading used to udpate/create the new Sequence of Items
-     * @param items is a list of Item that will be on the same Tile
-     * @return a new Sequence of Items with the correct Heading
+     * Given a {@code Heading} and a {@code Sequence of Items}, creates a new {@code Sequence of Items} whose fields have the same values as the parameter.
+     * Is implemented using the {@code Item.withHeading()} method.
+     * {@code Sequence of Items}, except for the {@code Heading} which is now equal to the parameter {@code Heading}.
+     * @param heading {@code Heading} of the Items in the returned {@code Sequence}
+     * @param items a {@code Sequence of Items} that will be on the same {@code Tile}
+     * @return a new {@code Sequence of Items} with the correct {@code Heading}
      */
     public static Sequence<Item> withHeadings(Heading heading, Sequence<Item> items) {
         return map(
