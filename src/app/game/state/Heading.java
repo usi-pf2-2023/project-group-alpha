50c6d40b8beb80c7d4f2c0866a46adaf9b1e9585
Jamila Oubenali <jkuma@protonmail.com>
2023-06-02 19:03:54 +0200

    feat: turning Heading into an interface

--- src/app/game/state/Heading.java
+++ src/app/game/state/Heading.java
@@ -3,12 +3,25 @@ package src.app.game.state;
 import jtamaro.en.io.KeyboardKey;
 
 // Heading is a model class. It's independent of the UI.
-public enum Heading {
+public interface Heading {
+
+    public abstract int newXCoordinate();
+    public abstract int newYCoordinate();
+
+    public static Heading newHeading(KeyboardKey key) {
+        int KEY_CODE = key.getCode();
+        return
+            KEY_CODE == KeyboardKey.UP ? new North() :
+            KEY_CODE == KeyboardKey.DOWN ? new South() :
+            KEY_CODE == KeyboardKey.RIGHT ? new East() :
+            new West();
+    }
+}
+/*
     NORTH,
     SOUTH,
     EAST,
     WEST;
-
     // convert a keycode to direction.
     public static Heading fromKeyCode(int keyCode) {
         return
@@ -18,4 +31,6 @@ public enum Heading {
             WEST;
     }
 
-}
+    // Heading.newX()
+*/
+
